d367527a0b605836962cf0953ceb10c48acddcef
fatemehhamzavi <147841711+fatemehhamzavi@users.noreply.github.com>
2023-10-13 21:16:58 +0330

    Add files via upload

---
+++ hamzavii.py
@@ -0,0 +1,54 @@
+f1=int(input("enter number:"))
+f2=int(input("enter number:"))
+
+print("sum=",f1+f2)
+print("subtraction=",f1-f2)
+print("multi=",f1*f2)
+print("division=",f1/f2)
+print("baghimandeh=",f1//f2)
+print("taghsim sahih",f1%f2)
+print("tavan=",f1**f2)
+
+
+
+f1=int(input("enter number:"))
+f2=int(input("enter number:"))
+
+print("and=",f1 and f2)
+print("or=",f2 or f2)
+print("&=",f2 & f2)
+print("|=",f2 | f2)
+print("^=",f2 ^ f2)
+
+
+f1=int(input("enter number:"))
+f2=int(input("enter number:"))
+
+print("bin+=",f1+f2)
+print("bin-=",f1-f2)
+print("bin*=",f1*f2)
+print("bin//=",f1//f2)
+print("bin%=",f1%f2)
+print("bin**=",f1**f2)
+
+
+f1=int(input("enter number:"))
+f2=int(input("enter number:"))
+
+print("oct+=",f1+f2)
+print("oct-=",f1-f2)
+print("oct*=",f1*f2)
+print("oct//=",f1//f2)
+print("oct%=",f1%f2)
+print("oct**=",f1**f2)
+
+
+f1=int(input("enter number:"))
+f2=int(input("enter number:"))
+
+print("hex+=",f1+f2)
+print("hex-=",f1-f2)
+print("hex*=",f1*f2)
+print("hex//=",f1//f2)
+print("hex%=",f1%f2)
+print("hex**=",f1**f2)
