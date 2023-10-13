c78c6ef711df032d223a2eb0c43ed4e3ca15c523
nooraref81 <147872667+nooraref81@users.noreply.github.com>
2023-10-14 02:37:21 +0330

    Add files via upload

---
+++ arefeh noorbakhsh.py
@@ -0,0 +1,45 @@
+i=input('place inter number1')
+i=int(i)
+x=input('place inter number2')
+x=int(x)
+g=i+x
+m=i-x
+z=i*x
+s=i%x
+w=i//x
+t=i**x
+q=i and x
+a=i or x
+d=i&x
+h=i|x
+u=i^x
+print("جمع",g)
+print("تفريق",m)
+print("ضرب",z)
+print("باقي مانده تقسيم",s)
+print("عدد صحيح تقسيم",w)
+print("توان",t)
+print("و منطقي",q)
+print("يا منطقي",a)
+print("و بيتي",d)
+print("يا بيتي",h)
+print("يا انحصاري بيتي",u)
+print("حاصل جمع باينري",bin(g))
+print("حاصل تفريق باينري",bin(m))
+print("حاصل ضرب باينري",bin(z))
+print("باقي مانده تقسيم به باينري",bin(s))
+print("عدد صحيح تقسيم به باينري",bin(w))
+print("توان به باينري",bin(t))
+print("حاصل جمع مبناي هشت",oct(g))
+print("حاصل تفريق مبناي هشت",oct(m))
+print("حاصل ضرب در مبناي هشت",oct(z))
+print("باقي مانده تقسيم مبناي هشت",oct(s))
+print("عدد صحيح تقسيم در ميناي هشت",oct(w))
+print("توان در مبناي هشت",oct(t))
+print("حاصل جمع در مبناي شانزده",hex(g))
+print("حاصل تفريق در مبناي شانزده",hex(m))
+print("حاصل ضرب در مبناي شانزده",hex(z))
+print("باقي مانده تقسيم در مبناي شانزده",hex(s))
+print("عدد صحيح تقسيم در مبناي شانزده",hex(w))
+print("توان در مبناي شانزده",hex(t))
+
