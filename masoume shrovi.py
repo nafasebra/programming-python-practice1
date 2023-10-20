b2b5bd351dee74d2377ce2edbbcc5c77e909bc2a
Masoomehs <147873220+Masoomehs@users.noreply.github.com>
2023-10-20 22:44:21 +0330

    Add files via upload

---
+++ masoume shrovi.py
@@ -0,0 +1,15 @@
+n=int (input('enter number: '))
+if n%7==0:
+    print('شنبه')
+elif n%7==1:
+    print('يک شنبه ')
+elif n%7==2:
+    print('دو شنبه')
+elif n%7==3:
+    print('سه شنبه')
+elif n%7==4:
+    print('چهار شنبه')
+elif n%7==5:
+    print('پنج شنبه')
+else:
+    print('جمعه')
