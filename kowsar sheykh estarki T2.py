9361fd57710eabe48017cf3af5738bbd1df3eaa6
Kowsar <147835336+Delvin8079@users.noreply.github.com>
2023-10-20 23:14:30 +0330

    Add files via upload

---
+++ kowsar sheykh estarki T2.py
@@ -0,0 +1,16 @@
+number=int(input('enter number:'))
+
+if(number%7==0):
+    print('shanbeh')
+elif(number%6==0):
+    print('yekshanbeh')
+elif(number%5==0):
+    print('doshanbeh')
+elif(number%4==0):
+    print('sehshanbeh')
+elif(number%3==0):
+    print('chaharshanbeh')
+elif(number%2==0):
+    print('panchshanbeh')
+else:
+    print('jomeh')
