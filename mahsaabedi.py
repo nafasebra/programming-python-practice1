1b223098d5db91aa84be2ac18a53f94ad87c38ba
mahsaabdiii2002 <147873142+mahsaabdiii2002@users.noreply.github.com>
2023-10-20 22:46:07 +0330

    Add files via upload

---
+++ mahsaabedi.py
@@ -0,0 +1,15 @@
+d=int (input('enter number: '))
+if d%7==0:
+    print('شنبه')
+elif d%7==1:
+    print('يک شنبه ')
+elif d%7==2:
+    print('دو شنبه')
+elif d%7==3:
+    print('سه شنبه')
+elif d%7==4:
+    print('چهار شنبه')
+elif d%7==5:
+    print('پنج شنبه')
+else:
+    print('جمعه')
