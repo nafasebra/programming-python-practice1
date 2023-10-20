adcb093ba872e444db79111e8b88e9c63fe817d9
Kpourakbari <147872638+Kpourakbari@users.noreply.github.com>
2023-10-20 22:44:04 +0330

    Add files via upload

---
+++ Weekdays-KosarPourakbari.py
@@ -0,0 +1,15 @@
+ruz=int (input('enter number: '))
+if ruz%7==0:
+    print('shanbe')
+elif ruz%7==1:
+    print('yek shanbe')
+elif ruz%7==2:
+    print('do shanbe')
+elif ruz%7==3:
+    print('se shanbe')
+elif ruz%7==4:
+    print('chahar shanbe')
+elif ruz%7==5:
+    print('panj shanbe')
+else:
+    print('jome')
