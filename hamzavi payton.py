62752bc2ada44049b983c9bf03bcbdac56430660
fatemehhamzavi <147841711+fatemehhamzavi@users.noreply.github.com>
2023-10-20 23:21:40 +0330

    Add files via upload

---
+++ hamzavi payton.py
@@ -0,0 +1,17 @@
+f=int(input("enter number:"))
+
+if f%7==0:
+    print("شنبه")
+elif f%7==1:
+    print("يکشنبه")
+elif f%7==2:
+    print("دوشنبه")
+elif f%7==3:
+    print("سه شنبه")
+elif f%7==4:
+    print("چهارشنبه")
+elif f%7==5:
+    print("پنجشنبه")
+else:
+    print("جمعه")
+    
