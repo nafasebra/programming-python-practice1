32aceff775951f1db70af0d6051a65745096cafe
Kowsar <147835336+Delvin8079@users.noreply.github.com>
2023-11-03 23:20:31 +0330

    Add files via upload

---
+++ mona ghanbari.py
@@ -0,0 +1,10 @@
+def charChange(str):
+    $=""
+    for i in range(0.len(str)):
+        s+=str[i].upper()
+    else:
+        s+=str[i].lower()
+    return s
+str=input('enter a string:')
+str=charChange(str)
+print('chapkon:'. str)
